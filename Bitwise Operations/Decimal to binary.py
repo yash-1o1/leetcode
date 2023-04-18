1187e737bce9411843a9400b6e39ef0071410782
Yash Singh <yashv99@vt.edu>
2023-04-17 23:37:02 -0400

    Bitwise operations updated.

--- Bitwise Operations/Decimal to binary.py
+++ Bitwise Operations/Decimal to binary.py
@@ -1,1 +1,19 @@
 # convert the given decimal to binary
+# Example 1:
+# Input: n = 11
+# Output: 1011
+# Explanation: The input decimal number 11 has a binary representation 1011.
+# Example 2:
+# Input: n = 128
+# Output: 10000000
+
+
+# the first approach is to use the following logic:
+# to convert decimal to binary, divide the decimal by 2 and store the remainder
+# repeat the process until the quotient is 0
+# the binary number is the sequence of remainders in reverse order
+#
+# we cam use another alternative approach to solve this problem:
+# to convert decimal to binary, divide the decimal by 2 and store the quotient
+# repeat the process until the quotient is 0
+# the binary number is the sequence of remainders in reverse order
